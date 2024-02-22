17db35a0e3c6039e287d9459160c226ea3a84417
Tom <tom@msbit.com.au>
2024-02-22 17:09:21 +1100

    chore: split up and update printing

---
+++ files.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+)
+
+func copy(entry fs.DirEntry, source string, target string) error {
+	t, err := fsTime(entry)
+	if err != nil {
+		return err
+	}
+
+	month := t.Format("2006-01")
+	target = fmt.Sprintf("%s/%s", target, month)
+
+	err = os.MkdirAll(target, 0750)
+	if err != nil {
+		return err
+	}
+
+	sourceFile := fmt.Sprintf("%s/%s", source, entry.Name())
+
+	r, err := os.Open(sourceFile)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	targetFile := fmt.Sprintf("%s/%s", target, entry.Name())
+
+	_, err = os.Stat(targetFile)
+	if err == nil {
+		return fs.ErrExist
+	}
+
+	if !errors.Is(err, os.ErrNotExist) {
+		return err
+	}
+
+	w, err := os.OpenFile(targetFile, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	if _, err = io.Copy(w, r); err != nil {
+		return err
+	}
+
+	return os.Chtimes(targetFile, t, t)
+}
--- main.go
+++ main.go
@@ -45,7 +45,10 @@ func main() {
 	for _, entries := range hashes {
 		entry := earliest(entries)
 
-		if err := copy(entry, source, target); err != nil {
+		err := copy(entry, source, target)
+		if errors.Is(err, fs.ErrExist) {
+			log.Printf("not copying %s file exists", entry.Name())
+		} else if err != nil {
 			log.Fatal(err)
 		}
 	}
@@ -75,52 +78,4 @@ func earliest(entries []fs.DirEntry) fs.DirEntry {
 			mustFsTime(b).UnixNano(),
 		)
 	})
-}
-
-func copy(entry fs.DirEntry, source string, target string) error {
-	t, err := fsTime(entry)
-	if err != nil {
-		return err
-	}
-
-	month := t.Format("2006-01")
-	target = fmt.Sprintf("%s/%s", target, month)
-
-	err = os.MkdirAll(target, 0750)
-	if err != nil && !os.IsExist(err) {
-		return err
-	}
-
-	sourceFile := fmt.Sprintf("%s/%s", source, entry.Name())
-
-	r, err := os.Open(sourceFile)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	targetFile := fmt.Sprintf("%s/%s", target, entry.Name())
-
-	_, err = os.Stat(targetFile)
-
-	if err == nil {
-		log.Printf("not copying %s file exists", entry.Name())
-		return nil
-	}
-
-	if !errors.Is(err, os.ErrNotExist) {
-		return err
-	}
-
-	w, err := os.OpenFile(targetFile, os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		return err
-	}
-	defer w.Close()
-
-	if _, err = io.Copy(w, r); err != nil {
-		return err
-	}
-
-	return os.Chtimes(targetFile, t, t)
 }
