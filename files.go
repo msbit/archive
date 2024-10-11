2d1253f79ec699640ba0783a519cedb6dd12307e
tom <tom@msbit.com.au>
2024-10-11 15:47:41 +1100

    refactor: extract `copyDir`

--- files.go
+++ files.go
@@ -1,12 +1,77 @@
 package main
 
 import (
+	"cmp"
+	"crypto/md5"
 	"errors"
 	"fmt"
 	"io"
 	"io/fs"
+	"log"
 	"os"
+	"slices"
 )
+
+func copyDir(
+	source string,
+	target string,
+) error {
+	entries, err := os.ReadDir(source)
+	if err != nil {
+		return fmt.Errorf("unable to read dir: %w", err)
+	}
+
+	hashes := make(map[string][]fs.DirEntry)
+	log.Printf("hashing %d entries (including directories)", len(entries))
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+
+		name := fmt.Sprintf("%s/%s", source, entry.Name())
+		h, err := hash(name)
+		if err != nil {
+			return fmt.Errorf("unable to hash file: %w", err)
+		}
+
+		hashes[h] = append(hashes[h], entry)
+	}
+
+	log.Printf("copying %d unique files", len(hashes))
+	for _, entries := range hashes {
+		entry := earliest(entries)
+
+		if err := copy(entry, source, target); err != nil {
+			log.Printf("not copying %s: %s", entry.Name(), err)
+		}
+	}
+
+	return nil
+}
+
+func hash(name string) (string, error) {
+	r, err := os.Open(name)
+	if err != nil {
+		return "", err
+	}
+	defer r.Close()
+
+	w := md5.New()
+	if _, err := io.Copy(w, r); err != nil {
+		return "", err
+	}
+
+	return string(w.Sum(nil)), nil
+}
+
+func earliest(entries []fs.DirEntry) fs.DirEntry {
+	return slices.MinFunc(entries, func(a, b fs.DirEntry) int {
+		return cmp.Compare(
+			mustFsTime(a).UnixNano(),
+			mustFsTime(b).UnixNano(),
+		)
+	})
+}
 
 func copy(
 	entry fs.DirEntry,
--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"cmp"
-	"crypto/md5"
-	"fmt"
-	"io"
-	"io/fs"
 	"log"
 	"os"
-	"slices"
 )
 
 func main() {
@@ -16,62 +10,10 @@ func main() {
 		log.Fatalf("usage: %s <source> <target>", os.Args[0])
 	}
 
-	source := os.Args[1]
-	target := os.Args[2]
-
-	entries, err := os.ReadDir(source)
-	if err != nil {
+	if err := copyDir(
+		os.Args[1],
+		os.Args[2],
+	); err != nil {
 		log.Fatal(err)
 	}
-
-	hashes := make(map[string][]fs.DirEntry)
-	log.Printf("hashing %d entries (including directories)", len(entries))
-	for _, entry := range entries {
-		if entry.IsDir() {
-			continue
-		}
-
-		name := fmt.Sprintf("%s/%s", source, entry.Name())
-		h, err := hash(name)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		hashes[h] = append(hashes[h], entry)
-	}
-
-	log.Printf("copying %d unique files", len(hashes))
-	for _, entries := range hashes {
-		entry := earliest(entries)
-
-		if err := copy(entry, source, target); err != nil {
-			log.Printf("not copying %s: %s", entry.Name(), err)
-		}
-	}
-
-	log.Printf("done")
-}
-
-func hash(name string) (string, error) {
-	r, err := os.Open(name)
-	if err != nil {
-		return "", err
-	}
-	defer r.Close()
-
-	w := md5.New()
-	if _, err := io.Copy(w, r); err != nil {
-		return "", err
-	}
-
-	return string(w.Sum(nil)), nil
-}
-
-func earliest(entries []fs.DirEntry) fs.DirEntry {
-	return slices.MinFunc(entries, func(a, b fs.DirEntry) int {
-		return cmp.Compare(
-			mustFsTime(a).UnixNano(),
-			mustFsTime(b).UnixNano(),
-		)
-	})
 }
